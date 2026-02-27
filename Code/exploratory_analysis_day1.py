9083fcb074ad739f46e705f001488e5563754039
MA806-commits <makaylaadams806@gmail.com>
2026-02-27 18:59:58 -0500

    Day 1 Question Responses

--- Code/exploratory_analysis_day1.py
+++ Code/exploratory_analysis_day1.py
@@ -17,3 +17,8 @@ plt.title('Active Cases of Mystery Virus Over Time')
 plt.plot(data['day'], data['active reported daily cases'], marker='o', linestyle='-')
 
 plt.show()
+
+## Response to Questions: 
+# What do you notice about the initial infections?: The initial phase (Days 0-20) shows exponential growth. At the beginning, the number of active cases stays very low for nearly 10 days, making the threat appear minimal. Despite the low numbers, the looks to be doubling at a constant rate. Around Day 20, the curve bends sharply upwards. 
+# How could we measure how quickly its spreading?: We could measure the growth rate (r) of the exponential curve during the initial phase. This growth rate can be used to estimate the basic reproduction number (R0) of the virus, which indicates how many people, on average, one infected person will infect in a fully susceptible population. By looking at the time between the first exposure and the first sharp spike, you can estimate the virus's incubation period. 
+# What information about the virus would be helpful in determining the shape of the outbreak curve?: Information about the virus's incubation period, infectious period, and transmission mode would be helpful. Also, data on population density, social behavior, and intervention measures (like social distancing) would also influence the shape of the curve. 
--- Code/exploratory_analysis_day2.py
+++ Code/exploratory_analysis_day2.py
@@ -2,20 +2,40 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy
 from scipy.optimize import curve_fit
 #%%
 # Load the data
-data = pd.read_csv('../Data/mystery_virus_daily_active_counts_RELEASE#1.csv', parse_dates=['date'], header=0, index_col=None)
+data = pd.read_csv('Module-2-Epidemics-SIR-Modeling/Data/mystery_virus_daily_active_counts_RELEASE#1.csv', parse_dates=['date'], header=0, index_col=None)
 #%%
 # We have day number, date, and active cases. We can use the day number and active cases to fit an exponential growth curve to estimate R0.
 # Let's define the exponential growth function
 def exponential_growth(t, r):
     return np.exp(r * t)
 
-# Fit the exponential growth model to the data. 
+# Copying over the plot from day 1
+####Labeling 
+plt.xlabel('Day')
+plt.ylabel('Active Cases')
+plt.title('Active Cases of Mystery Virus Over Time')
+
+####Loading the data
+plt.plot(data['day'], data['active reported daily cases'], marker='o', linestyle='-')
+
+plt.show()
+# Fit the exponential growth model to the data.
+p0 = [0.1]  # Initial guess for the growth rate
+params, cv = scipy.optimize.curve_fit(exponential_growth, data['day'], data['active reported daily cases'], p0=p0)
+
+# Hint: Look up the documentation for curve_fit to see how to use it. You will need to provide the day numbers and active cases as inputs, and the exponential_growth function as the model to fit. 
 # We'll use a handy function from scipy called CURVE_FIT that allows us to fit any given function to our data. 
 # We will fit the exponential growth function to the active cases data. HINT: Look up the documentation for curve_fit to see how to use it.
 
 # Approximate R0 using this fit
+R0 = params[0]
+print(f"Estimated R0: {R0}")
 
 # Add the fit as a line on top of your scatterplot.
+plt.plot(data['day'], data['active reported daily cases'], marker='o', linestyle='-', label='Data')
+plt.plot(data['day'], exponential_growth(data['day'], params[0]), label='Exponential Fit')
+plt.title("Fitted Exponential Growth Curve")
